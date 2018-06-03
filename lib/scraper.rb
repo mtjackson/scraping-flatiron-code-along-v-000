99a92c119b4234c9034000655b0d6bcb5e7698c0
Mary Jackson <jackson.marytherese@gmail.com>
2018-06-03 17:07:12 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -26,7 +26,7 @@ class Scraper
   end
 
   def make_courses
-    doc.css(".post").each do |post|
+    self.get_cour.each do |post|
       course = Course.new
       course.title = post.css("h2").text
       course.schedule = post.css(".date").text
